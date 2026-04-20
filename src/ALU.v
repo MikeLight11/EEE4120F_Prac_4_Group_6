63b16affc5885c9cf37da9f25dabad6593f36086
MikeLight11 <mlighton04@gmail.com>
2026-04-20 13:56:43 +0200

    Completed Code for ALU.v (task 1)

--- src/ALU.v
+++ src/ALU.v
@@ -5,8 +5,8 @@
 // GROUP NUMBER:
 //
 // MEMBERS:
-//   - Member 1 Name, Student Number
-//   - Member 2 Name, Student Number
+//   - Member 1 Michael Lighton, LGHMIC003
+//   - Member 2 Glen Jones, JNSGLE007
 
 // File        : ALU.v
 // Description : 16-bit Arithmetic and Logic Unit (ALU).
@@ -36,6 +36,8 @@ module ALU (
     //       assign zero = ...;
     // -------------------------------------------------------------------------
 
+    assign zero = (result == 16'd0);
+
     // -------------------------------------------------------------------------
     // TODO: Implement the ALU operations using a combinational always block.
     //
@@ -61,6 +63,22 @@ module ALU (
     //       treats reg/wire values as unsigned by default. This is correct
     //       for the StarCore ISA.
     // -------------------------------------------------------------------------
+
+    always @(*) begin
+        case (alu_control)
+            3'b000 : result = a + b; // ADD
+            3'b001 : result = a - b; // SUB
+            3'b010 : result = ~a; // INV
+            3'b011 : result = a << b[3:0]; // SHL
+            3'b100 : result = a >> b[3:0]; // SHR
+            3'b101 : result = a & b; // AND
+            3'b110 : result = a | b; // OR
+            3'b111 : result = (a < b) ? 16'd1 : 16'd0; // SLT
+            default: result = a + b; //default
+        endcase
+    end
+
+endmodule
 
 
 endmodule
